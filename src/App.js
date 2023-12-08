ac1732b0b1ae58ec980518065cf12d7c70f68dca
Mario <jorge.fernandez@pragma.com.co>
2023-12-08 10:36:18 -0500

    add first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title}></img>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              img={componentsImg}
+            ></CoreConcept>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              img={componentsImg}
+            ></CoreConcept>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              img={componentsImg}
+            ></CoreConcept>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              img={componentsImg}
+            ></CoreConcept>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
